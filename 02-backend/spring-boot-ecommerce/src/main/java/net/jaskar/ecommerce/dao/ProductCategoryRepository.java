81fc6d89e3730edd580da16b27202d0f17adf891
Jan Kucharski <jankucharski99@gmail.com>
2025-02-28 17:54:09 +0100

    Set up full-stack eCommerce app with Angular & Spring
    - Developed JPA entities and configured Spring Data REST APIs as read-only
    - Created Angular project and implemented Product and ProductCategory classes & Product service
    - Integrated Angular with Spring Boot for a full-stack setup
    - Built an HTML table with product images and installed Bootstrap styles via npm
    - Added local custom CSS styles and integrated template files & logos
    - Implemented product grid display
    - Added category-based product filtering (product-category-menu.component)
    - Implemented dynamic keyword search (search.component)

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/net/jaskar/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,13 @@
+package net.jaskar.ecommerce.dao;
+
+import net.jaskar.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+
+}
---
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: false,
+  templateUrl: './product-list-grid.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
+    
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    
+    if (hasCategoryId) {
+      // get the "id" param string, convert string to a number using "+" symbol
+      // '!' is the non-null assertion operator, it tells copiler that the object is not null
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // not catogory id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  
+  }
+}

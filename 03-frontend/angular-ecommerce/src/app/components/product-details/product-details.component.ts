6159f286b75ce783e2e7752736fdab9353d65b3d
Jan Kucharski <jankucharski99@gmail.com>
2025-02-28 17:44:45 +0100

    Add detail view
    - Created new ProductDetail component for displaying individual product details.
    - Defined route for product detail view.
    - Added router links in the master page to navigate to the product detail view.
    - Updated product.service to fetch product data by ID for the detail view.

---
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+
+const PROD_ID = 'prodId';
+const CAT_ID = 'catId';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: false,
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css'
+})
+export class ProductDetailsComponent implements OnInit{
+
+  product: Product = new Product();
+  categoryId: number = 1;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.categoryId = +this.route.snapshot.paramMap.get(CAT_ID)!;
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  handleProductDetails() {
+
+    // get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get(PROD_ID)!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+
+    this.productService.getProduct(theProductId);
+  }
+}

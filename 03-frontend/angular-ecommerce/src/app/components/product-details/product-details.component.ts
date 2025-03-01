3bc2e8e83d7bc5c0255395fc4eb7dbe3b4fdf3a1
Jan Kucharski <jankucharski99@gmail.com>
2025-03-02 00:46:33 +0100

    Add cart functionality
    - Create `cart-item` class to hold cart item data
    - Implement `cart-status.component` to display cart summary
    - Create `cart.service` to manage cart operations
    - Add click handler for adding items to cart
    - Subscribe to `cart-service` in `cart-status.component` to display totals
    - Enable adding items to cart from product details view

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
 
 const PROD_ID = 'prodId';
 const CAT_ID = 'catId';
@@ -19,6 +21,7 @@ export class ProductDetailsComponent implements OnInit{
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
   ) { }
 
@@ -41,5 +44,11 @@ export class ProductDetailsComponent implements OnInit{
     )
 
     this.productService.getProduct(theProductId);
+  }
+
+  addToCart() {
+
+    console.log(`Adding to cart; ${this.product.name}, ${this.product.unitPrice}`);
+    this.cartService.addToCart(new CartItem(this.product));
   }
 }

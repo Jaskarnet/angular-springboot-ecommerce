f407142c2511dc2c158d83101248bf8749538054
Jan Kucharski <jankucharski99@gmail.com>
2025-03-01 18:39:44 +0100

    Add pagination support to product listing
    - Installed ng-bootstrap for pagination components.
    - Updated ProductService to support pagination for product list retrieval.
    - Enhanced ProductListComponent to handle paginated product data.
    - Modified HTML template to integrate ng-bootstrap pagination component.
    - Added functionality for selecting page size in the product list.
    - Updated search functionality to include pagination when searching by keyword.

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -36,7 +36,7 @@ export class ProductService {
   }
 
   private getProducts(searchUrl: string): Observable<Product[]> {
-    return this.httpClient.get<GetResponseProduct>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
@@ -47,11 +47,37 @@ export class ProductService {
 
     return this.httpClient.get<Product>(productUrl);
   }
+
+  getProductListPaginate( thePage: number, 
+                          thePageSize: number, 
+                          theCategoryId: number): Observable<GetResponseProducts> {
+    // need to build URL based on category id, page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
+  searchProductsPaginate( thePage: number, 
+                          thePageSize: number, 
+                          theKeyword: string): Observable<GetResponseProducts> {
+    // need to build URL based on keyword, page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
 }
 
-interface GetResponseProduct {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 

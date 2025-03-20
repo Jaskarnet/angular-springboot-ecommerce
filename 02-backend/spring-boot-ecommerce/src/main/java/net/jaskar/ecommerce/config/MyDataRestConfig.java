392ed164856bcdc390608a7f99d5b03c3e140ce8
Jan Kucharski <jankucharski99@gmail.com>
2025-03-20 19:35:10 +0100

    add order history functionality
    - Backend:
      - Create OrderRepository REST API to fetch orders by customer email, sorted by creation date (descending)
      - Implement query methods using Spring Data REST naming conventions
      - Configure OrderRepository as read-only
    
    - Frontend:
      - Store user email in browser storage
      - Create OrderHistory model class
      - Develop OrderHistoryService to communicate with backend
      - Generate OrderHistoryComponent to display past orders
      - Design HTML template to iterate over orderHistoryList
      - Add "Orders" button
      - Secure order history route with authentication

--- 02-backend/spring-boot-ecommerce/src/main/java/net/jaskar/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/net/jaskar/ecommerce/config/MyDataRestConfig.java
@@ -2,10 +2,7 @@ package net.jaskar.ecommerce.config;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
-import net.jaskar.ecommerce.entity.Country;
-import net.jaskar.ecommerce.entity.Product;
-import net.jaskar.ecommerce.entity.ProductCategory;
-import net.jaskar.ecommerce.entity.State;
+import net.jaskar.ecommerce.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -41,6 +38,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);

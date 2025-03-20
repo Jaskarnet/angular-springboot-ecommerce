f0dc52690466c639b53b197715ca36323c7fb6df
Jan Kucharski <jankucharski99@gmail.com>
2025-03-20 19:35:10 +0100

    add order history functionality
    - Backend:
      - Create OrderRepository REST API to fetch orders by customer email, sorted by creation date (descending)
      - Implement query methods using Spring Data REST naming conventions
      - Configure OrderRepository as read-only
    
    - Frontend:
      - Store user email in browser storage
      - Create OrderHistory model class
      - Develop OrderHistoryService to communicate with backend
      - Generate OrderHistoryComponent to display past orders
      - Design HTML template to iterate over orderHistoryList
      - Add "Orders" button
      - Secure order history route with authentication

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -35,6 +35,8 @@ export class CheckoutComponent {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
+
   constructor(private formBuilder: FormBuilder, 
               private luv2ShopFormService: Luv2ShopFormService,
               private cartService: CartService,
@@ -45,11 +47,14 @@ export class CheckoutComponent {
 
     this.reviewCartDetails();
 
+    // read the user's email address from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
         lastName: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
-        email: new FormControl('', 
+        email: new FormControl(theEmail, 
           [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}')]),
       }),
       shippingAddress: this.formBuilder.group({
